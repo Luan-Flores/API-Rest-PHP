09642b06ce0e805bfe864e73bd75ad55a1e79ce5
luan <keeganluan919@gmail.com>
2025-08-13 09:54:26 -0400

    # Implementação das rotas usando .http para testes e debug

---
+++ classes/rotas.php
@@ -0,0 +1,78 @@
+<?php
+
+class Rotas {
+    private $listaRotas = [''];
+    private $listaCallback = [''];
+    private $listaProtecao = [''];
+
+    public function add($method, $route, $callback, $protection){
+        $this->listaRotas[] = strtoupper($method).':'.$route;     
+        $this->listaCallback[] = $callback;
+        $this->listaProtecao[] = $protection;
+        
+        return $this;
+    }
+    public function ir($route){
+        $param = '';
+        $callback = '';
+        $protection = '';
+        $methodServer = $_SERVER['REQUEST_METHOD'];
+
+        // $methodServer = isset($_POST['_method']) ? $_POST['_method'] : $methodServer;
+        // null coalescing operator 
+        $methodServer = $_POST['_method'] ?? $methodServer;
+        $route = $methodServer.':/'.$route;
+
+        
+        if (substr_count($route, '/') >= 3){
+            $param = substr($route, strrpos($route, "/") + 1);
+            $route = substr($route, 0, strrpos($route, "/"))."/[PARAM]";
+        }
+        var_dump($route);
+      
+       
+
+
+        $index = array_search($route, $this->listaRotas);
+        if ($index > 0) {
+            $callback = explode('::', $this->listaCallback[$index]);
+            $protection = $this->listaProtecao[$index];
+        }
+        
+        $class = $callback[0] ?? '';
+        $method = $callback[1] ?? '';
+
+
+        
+        if (class_exists($class))
+            {
+            if(method_exists($class, $method))
+                {
+                $instanciaClass = new $class();
+                if($protection){
+                    $verify = new Usuarios();
+                    if($verify->verificar()){
+                        return call_user_func_array(
+                            array($instanciaClass, $method),
+                            array($param)
+                        );
+                    } else{
+                        echo json_encode(["DADOS" => "Token inválido"]);
+                    }
+                }else{
+                    return call_user_func_array(
+                        array($instanciaClass, $method),
+                        array($param)
+                    );
+                }
+            } else{
+                $this->naoExiste();
+            }
+        }else{
+            $this->naoExiste();
+        }
+    }
+    public function naoExiste(){
+        http_response_code(404);
+    }
+}
--- index.php
+++ index.php
@@ -9,25 +9,26 @@ date_default_timezone_set("America/Sao_Paulo"); //Definindo o horário
 
 
 //Verifica se o parâmetro path foi enviado na URL (via .htaccess ou manualmente com ?path=alguma/coisa).
-if (isset($_GET['path'])) {
-    $path = explode('/', $_GET['path']);
-} else {
-    echo "Caminho inexistente 1"; exit;
-}    
-if (isset($path[0])) { $api = $path[0]; } else {echo "Caminho inexistente "; exit;}
-// Verifica se existe a primeira parte da URL (índice 0).
-// Salva em $api, que representa o "nome do recurso".
-// Exemplo: "usuarios" (ex: api/usuarios/listar/123).
+// if (isset($_GET['path'])) {
+//     $path = explode('/', $_GET['path']);
+// } else {
+//     echo "Caminho inexistente 1"; exit;
+// }    
+// if (isset($path[0])) { $api = $path[0]; } else {echo "Caminho inexistente "; exit;}
+// // Verifica se existe a primeira parte da URL (índice 0).
+// // Salva em $api, que representa o "nome do recurso".
+// // Exemplo: "usuarios" (ex: api/usuarios/listar/123).
 
-if (isset($path[1])) { $acao = $path[1]; } else {$acao = ''; }
-// Verifica se existe a segunda parte da URL.
-// Salva em $acao, que representa o que será feito (ex: "listar" ou "criar").
+// if (isset($path[1])) { $acao = $path[1]; } else {$acao = ''; }
+// // Verifica se existe a segunda parte da URL.
+// // Salva em $acao, que representa o que será feito (ex: "listar" ou "criar").
 
-if (isset($path[2])) { $param = $path[2]; } else { $param = ''; }
-//Verifica se existe a terceira parte da URL.
-//Salva em $param, que geralmente é um ID ou parâmetro específico (ex: "123").
+// if (isset($path[2])) { $param = $path[2]; } else { $param = ''; }
+// //Verifica se existe a terceira parte da URL.
+// //Salva em $param, que geralmente é um ID ou parâmetro específico (ex: "123").
 
-$method = $_SERVER['REQUEST_METHOD'];
+// metodo ficou na classe rotas
+// $method = $_SERVER['REQUEST_METHOD'];
 // Captura o método HTTP da requisição (GET, POST, PUT, DELETE, etc).
 // Isso é útil para definir o tipo de ação que será tomada.
 // Por exemplo:
@@ -41,6 +42,16 @@ $GLOBALS['secretJWT'] = '123456';
 // echo password_hash('123456', PASSWORD_DEFAULT);
 // var_dump($method); //Apenas para debug
 
+# Autoload
 include_once "classes/autoload.class.php";
 new Autoload();
+
+# Rotas
+$rota = new Rotas();
+$rota->add('POST', '/usuarios/login', 'Usuarios::login', false);
+$rota->add('GET', '/clientes/listar', 'Clientes::listarTodos', true);
+$rota->add('GET', '/clientes/listar/[PARAM]', 'Clientes::listarUnico', true);
+$rota->add('PUT', '/clientes/atualizar/[PARAM]', 'Clientes::atualizar', true);
+
+$rota->ir($_GET['path']);
 
