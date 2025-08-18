1359ec7d2fad2c4281bf8773abb9c852c7a12490
luan <keeganluan919@gmail.com>
2025-08-18 09:32:42 -0400

    Atualiza DB para PostgreSQL e ajustes na API

--- classes/db.class.php
+++ classes/db.class.php
@@ -1,11 +1,23 @@
 <?php
 class DB {
     public static function connect(){
-        $host='';
-        $user='root';
-        $pass='';
-        $base='api';
+        $host   = "dpg-d2hhdrjipnbc73dqvf50-a.oregon-postgres.render.com";
+        $port   = "5432";
+        $user   = "banco1_ezys_user";
+        $pass   = "Vckemrx37TjQjFfIv4obZFGJ56aRZwKW"; // senha do Render
+        $dbname = "banco1_ezys";
 
-        return new PDO("mysql:host={$host};dbname={$base};charset=UTF8;", $user, $pass);
+        try {
+            $pdo = new PDO(
+                "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
+                $user,
+                $pass
+            );
+            // Opcional: modo de erros com exceção
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Erro ao conectar: " . $e->getMessage());
+        }
     }
 }
