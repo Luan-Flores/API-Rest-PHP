6373d75f6a5c6343d7347e9f85976188cb44e40d
luan <keeganluan919@gmail.com>
2025-08-20 08:34:58 -0400

    alteracoes/adicoes no banco de dados - adicionando tabela servicos e reservas

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *'); //Permitir acesso para todos endereços
-
 header('Content-type: application/json'); //Api retorna dados em JSON
-
 date_default_timezone_set("America/Sao_Paulo"); //Definindo o horário
-
 // Permitir métodos necessários
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
 // Permitir headers enviados pelo frontend
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // Se a requisição for OPTIONS, apenas responde 200 e encerra
@@ -16,36 +12,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 // var_dump($_GET['path']); //Tudo que vem da URL com parametro 'path'
-
-
-//Verifica se o parâmetro path foi enviado na URL (via .htaccess ou manualmente com ?path=alguma/coisa).
-// if (isset($_GET['path'])) {
-//     $path = explode('/', $_GET['path']);
-// } else {
-//     echo "Caminho inexistente 1"; exit;
-// }    
-// if (isset($path[0])) { $api = $path[0]; } else {echo "Caminho inexistente "; exit;}
-// // Verifica se existe a primeira parte da URL (índice 0).
-// // Salva em $api, que representa o "nome do recurso".
-// // Exemplo: "usuarios" (ex: api/usuarios/listar/123).
-
-// if (isset($path[1])) { $acao = $path[1]; } else {$acao = ''; }
-// // Verifica se existe a segunda parte da URL.
-// // Salva em $acao, que representa o que será feito (ex: "listar" ou "criar").
-
-// if (isset($path[2])) { $param = $path[2]; } else { $param = ''; }
-// //Verifica se existe a terceira parte da URL.
-// //Salva em $param, que geralmente é um ID ou parâmetro específico (ex: "123").
-
-// metodo ficou na classe rotas
-// $method = $_SERVER['REQUEST_METHOD'];
-// Captura o método HTTP da requisição (GET, POST, PUT, DELETE, etc).
-// Isso é útil para definir o tipo de ação que será tomada.
-// Por exemplo:
-// GET → buscar dados
-// POST → inserir
-// PUT → atualizar
-// DELETE → remover
 
 // senha de criptografia para o TOKEN
 $GLOBALS['secretJWT'] = '123456';
