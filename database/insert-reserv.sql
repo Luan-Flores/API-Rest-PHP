72c6dc9df61d72fa90b57ca1b49cfe67cdc93f1f
luan <keeganluan919@gmail.com>
2025-09-09 09:03:37 -0400

    # insert default reservs for testing

---
+++ database/insert-reserv.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO reservas (idCliente, idServico, dataHora, mensagem, status)
+VALUES 
+(34, 1, '2025-09-10 14:30:00', 'Preciso de atendimento rápido', 'Pendente');
+
+INSERT INTO reservas (idCliente, idServico, dataHora, mensagem, status)
+VALUES 
+(34, 2, '2025-09-12 09:00:00', 'Gostaria de confirmar detalhes do serviço', 'Em andamento');
+
+INSERT INTO reservas (idCliente, idServico, dataHora, mensagem, status)
+VALUES 
+(34, 3, '2025-09-15 16:45:00', 'Agendamento feito via sistema', 'Concluída');
