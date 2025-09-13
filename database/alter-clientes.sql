3ba29db2941975123cb63524aa14d2a5962499c1
luan <keeganluan919@gmail.com>
2025-09-13 10:29:25 -0400

    birthday email - feature

--- database/alter-clientes.sql
+++ database/alter-clientes.sql
@@ -1,1 +1,9 @@
-ALTER TABLE clientes ADD dataNasc DATE
+--ALTER TABLE clientes ADD dataNasc DATE
+insert into clientes (nome, email, dataNasc, cidade, estado, telefone) values (
+'Gusttavo Lima',
+'embaixador@gmail.com',
+'2005-09-13',
+'itumbiara',
+'GO',
+'679123000000'
+);
